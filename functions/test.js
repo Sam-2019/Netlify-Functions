ace6042d29fa4a0cffd70768f8e156394a13dc19
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-02-18 20:00:16 +0000

    code update

---
+++ functions/test.js
@@ -0,0 +1,13 @@
+exports.handler = (event, context, callback) => {
+  callback(null, {
+    statusCode: 200,
+    body: "No worries, all is working fine!",
+  });
+};
+
+// export async function handler(event, context) {
+//     return {
+//       statusCode: 200,
+//       body: JSON.stringify({ message: `Hello world ${Math.floor(Math.random() * 10)}` })
+//     };
+//   }
